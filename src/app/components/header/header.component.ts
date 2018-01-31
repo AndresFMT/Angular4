5562d02a3c253ed5461c938e710b8fe79d56c5b5
AFMARQUE <afmarque@AFMARQUE.local>
2018-01-31 18:02:33 -0500

    buscador

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,6 +10,15 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+		let _word = document.getElementById("ee-search");
+		let _button = document.getElementById("btn-search");
+		_button.addEventListener('click',_search);
+		function _search(){
+			
+	  	console.log(_word.value);
+	  	var url = 'https://www.emagister.com.co/web/search/?pfichas=14846&sid=3&utm_source=elespectador&utm_medium=cpl&utm_content=buscador_widget_edu&utm_campaign=buscador_widget_edu&btnBuscar=Buscar&q='+_word.value;
+	  	window.open(url,'_blank');
+		}
   }
 
 }
