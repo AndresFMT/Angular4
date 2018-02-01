2642997af109c4f81dd1eb20ea694d671ff7e5e6
AFMARQUE <afmarque@AFMARQUE.local>
2018-02-01 10:56:19 -0500

    buscador redirect

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,24 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
-		let _word = document.getElementById("ee-search");
-		let _button = document.getElementById("btn-search");
-		_button.addEventListener('click',_search);
-		function _search(){
-			
-	  	console.log(_word.value);
-	  	var url = 'https://www.emagister.com.co/web/search/?pfichas=14846&sid=3&utm_source=elespectador&utm_medium=cpl&utm_content=buscador_widget_edu&utm_campaign=buscador_widget_edu&btnBuscar=Buscar&q='+_word.value;
+  constructor( private router:Router) { }
+  search_Url( termino:string){
+  	
+	  	var url = 'https://www.emagister.com.co/web/search/?pfichas=14846&sid=3&utm_source=elespectador&utm_medium=cpl&utm_content=buscador_widget_edu&utm_campaign=buscador_widget_edu&btnBuscar=Buscar&q=' + termino;
 	  	window.open(url,'_blank');
 		}
-  }
 
 }
